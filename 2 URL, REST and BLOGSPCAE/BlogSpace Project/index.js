a07659eb8514ae9cb05c074470ee5201e3859e38
Asad <hellohello51981@gmail.com>
2025-05-18 11:35:44 +0500

    BlogSPace - Display post on webpage

--- 2 URL, REST and BLOGSPCAE/BlogSpace Project/index.js
+++ 2 URL, REST and BLOGSPCAE/BlogSpace Project/index.js
@@ -2,7 +2,16 @@
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
 .then(response => response.json())
 .then(data => {
-    console.log(data)
     const postsArray = data.slice(0 , 5)
     console.log(postsArray)
+    let html = ""
+
+    for (let post of postsArray){
+        html += `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <hr>
+        `
+    }
+    document.getElementById("blog-list").innerHTML = html
     })
