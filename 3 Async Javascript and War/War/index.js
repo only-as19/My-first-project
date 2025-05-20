b65cd6038b8949162c7cf3ee1cf060675bf50694
Asad <hellohello51981@gmail.com>
2025-05-20 12:01:29 +0500

    War Game - Install Tailwind

---
+++ 3 Async Javascript and War/War/index.js
@@ -0,0 +1,31 @@
+
+let deckId
+
+function handleClick(){
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle")
+    .then(response => response.json())
+    .then(data =>{ 
+        console.log(data)
+        deckId = data.deck_id
+        console.log(deckId)
+
+})
+}
+
+document.getElementById("new-deck").addEventListener('click', handleClick)
+
+
+document.getElementById("draw-card").addEventListener('click', () =>{
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.cards)
+        document.getElementById('cards').innerHTML = `
+        <img src=${data.cards[0].image} />
+        <img src=${data.cards[1].image} />
+        `
+    })
+})
+
+
+
