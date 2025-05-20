c0e7454393cb924fa4cd518cb15af8dfac4166bc
Asad <hellohello51981@gmail.com>
2025-05-20 11:02:05 +0500

    War Game - save deck Id for later use

--- 3 Async Javascript and War/War Game/index.js
+++ 3 Async Javascript and War/War Game/index.js
@@ -1,12 +1,20 @@
 
+let deckId
 
-// function handleClick(){
-//     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle")
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-// }
+function handleClick(){
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle")
+    .then(response => response.json())
+    .then(data =>{ 
+        console.log(data)
+        deckId = data.deck_id
+        console.log(deckId)
 
-// document.getElementById("new-deck").addEventListener('click', handleClick)
+})
+}
+
+document.getElementById("new-deck").addEventListener('click', handleClick)
+
+
 
 
 
