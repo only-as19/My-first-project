2ccf96d179f6ca96ddb7c81775b47036337d378c
Asad <hellohello51981@gmail.com>
2025-05-21 22:36:09 +0500

    War Game - Refactor code

--- 3 Async Javascript and War/War/index.js
+++ 3 Async Javascript and War/War/index.js
@@ -3,6 +3,7 @@ let deckId
 const cardContainer =  document.getElementById('cards')
 const newDeck = document.getElementById("new-deck")
 const drawCard = document.getElementById("draw-card")
+const callWinner = document.getElementById("callWinner")
 
 
 function handleClick(){
@@ -30,8 +31,25 @@ drawCard.addEventListener('click', () =>{
        cardContainer.children[1].innerHTML = `
         <img src=${data.cards[1].image} class="h-full w-full" />
         `
+        const winnerText = determineWinner(data.cards[0],data.cards[1])
+        console.log(winnerText)
+        callWinner.textContent = winnerText
     })
 })
 
 
+function determineWinner(card1,card2){
+    const valueOption = ["2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING","ACE",]
+    const card1ValueIndex = valueOption.indexOf(card1.value)
+    const card2ValueIndex = valueOption.indexOf(card2.value)
+    if(card1ValueIndex > card2ValueIndex){
+        return "Card 1 wins"
+    }else if(card1ValueIndex < card2ValueIndex){
+        return "Card 2 wins"
+    }else{
+        return "War"
+    }
+}
 
+const card1Obj = { value: "7" };
+const card2Obj = { value: "9" };
