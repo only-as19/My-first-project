31e5d0138696771b9da90469ca0f4fa416d41a4b
Asad <hellohello51981@gmail.com>
2025-05-24 13:38:16 +0500

    Get the author name form api

--- 4 Promises Rejection and Capstone/index.js
+++ 4 Promises Rejection and Capstone/index.js
@@ -1,8 +1,12 @@
+const author = document.getElementById("author")
+
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
   .then(res => res.json())
   .then(data => {
     console.log(data)
     document.body.style.backgroundImage = `url(${data.urls.full})`
+    author.innerText = `By : ${data.user.name}`
+
   })
 
   
