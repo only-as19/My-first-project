47c9e926c2ca914898eda775c30c5bd9181b63c8
Asad <hellohello51981@gmail.com>
2025-05-26 22:30:01 +0500

    Fetch Currency - First handle the rejcetion for fetching

--- 4 Promises Rejection and Capstone/index.js
+++ 4 Promises Rejection and Capstone/index.js
@@ -4,9 +4,26 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
   .then(res => res.json())
   .then(data => {
     console.log(data)
-    document.body.style.backgroundImage = `url(${data.urls.full})`
-    author.innerText = `By : ${data.user.name}`
+    document.body.style.backgroundImage = `url(bg.jpg)`
+    author.innerText = `By : Asad`
+    
 
   })
+  .catch(err => {
+    console.log("some")
+    document.body.style.backgroundImage = `url(${data.urls.full})`
+    author.innerText = `By : Jason Ortego`
+  })
 
+fetch91("https://api.coingecko.com/api/v3/coins/dogecoin")
+.then(res => {
+  if(!res.ok){
+    throw Error("Somthing went Wrong")
+  }
+  return res.json()
+})
+.then(data => {
+  
+})
+.catch(er => console.error(err))
   
